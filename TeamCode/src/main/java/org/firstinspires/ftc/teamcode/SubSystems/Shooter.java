5b685d78e8e33cd9d6848e4222a6668b43debe7d
VikyGoat <bethivikaas@gmail.com>
2026-09-13 19:19:41 -0400

    f

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SubSystems/Shooter.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SubSystems/Shooter.java
@@ -1,4 +1,28 @@
 package org.firstinspires.ftc.teamcode.SubSystems;
 
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
 public class Shooter {
+
+    private DcMotorEx shooterMotor;
+    private final double SHOOTER_VELOCITY = 1800; // ticks per second, tune this later
+
+    public void init(HardwareMap hardwareMap) {
+        shooterMotor = hardwareMap.get(DcMotorEx.class, "shooter");
+        shooterMotor.setDirection(DcMotor.Direction.FORWARD);
+    }
+
+    public void shoot() {
+        shooterMotor.setVelocity(SHOOTER_VELOCITY);
+    }
+
+    public void stop() {
+        shooterMotor.setVelocity(0);
+    }
+
+    public double getCurrentVelocity() {
+        return shooterMotor.getVelocity();
+    }
 }
